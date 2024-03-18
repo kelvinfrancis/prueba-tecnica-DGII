e1740d6b0fefa7ff546ed37d9d6286c7adb6c57a
KelvinMoquete <kelvinmoquete0597@gmail.com>
2024-03-18 16:37:08 -0400

    Eliminacion de librerias no utilizadas

--- Controllers/Contribuyentes.cs
+++ Controllers/Contribuyentes.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
